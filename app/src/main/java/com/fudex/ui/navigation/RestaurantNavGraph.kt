2eff0ff479162ccdd1471fe00e2630ce1a2dc100
Uriel Thoma <urielct2022@gmail.com>
2025-11-06 16:40:04 -0300

    Create MyCategoriesScreen

--- app/src/main/java/com/fudex/ui/navigation/RestaurantNavGraph.kt
+++ app/src/main/java/com/fudex/ui/navigation/RestaurantNavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.fudex.ui.components.AppScaffold
 import com.fudex.ui.screens.home.HomeScreen
+import com.fudex.ui.screens.mycategories.MyCategoriesScreen
 import com.fudex.ui.screens.profile.ProfileScreen
 
 fun NavGraphBuilder.restaurantNavGraph(navController: NavHostController) {
@@ -16,7 +17,8 @@ fun NavGraphBuilder.restaurantNavGraph(navController: NavHostController) {
 
         composable("restaurant_home"){
             //HomeScreen("Restaurant Home")
-            ProfileScreen()
+            //ProfileScreen()
+            MyCategoriesScreen()
         }
 
 
---
+++ app/src/main/java/com/fudex/ui/screens/mycategories/MyCategoriesViewModel.kt
@@ -0,0 +1,10 @@
+package com.fudex.ui.screens.mycategories
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyCategoriesViewModel @Inject constructor() : ViewModel() {
+
+}
